a79188775906c70ed4ffe8deb6d4aab8ee3c8d49
IrsyadPrasetya02 <irsyadprasetya02@gmail.com>
2025-09-26 09:37:10 +0700

    Add files via upload

---
+++ simpan.php
@@ -0,0 +1,43 @@
+<?php
+$koneksi = new mysqli("localhost", "root", "", "yata");
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
+}
+
+if (isset($_POST['simpan'])) {
+    $nama_produk = $_POST['nama_produk'];
+    $kategori = $_POST['kategori'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga = $_POST['harga'];
+    $gender = $_POST['gender'];
+    $stok = $_POST['stok'];
+    $ukuran = $_POST['ukuran'];
+    $warna = $_POST['warna'];
+    $fitur = isset($_POST['fitur']) ? implode(", ", $_POST['fitur']) : "";
+
+    // --- Tambahan cek folder upload ---
+    $targetDir = "uploads/";
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0777, true); // bikin folder kalau belum ada
+    }
+
+    $foto = "";
+    if (!empty($_FILES['foto']['name'])) {
+        $foto = $targetDir . basename($_FILES['foto']['name']);
+        if (move_uploaded_file($_FILES['foto']['tmp_name'], $foto)) {
+            echo "Upload berhasil<br>";
+        } else {
+            echo "Gagal upload file<br>";
+        }
+    }
+
+    $sql = "INSERT INTO produk (nama_produk, kategori, deskripsi, harga, gender, stok, ukuran, warna, fitur, foto) 
+            VALUES ('$nama_produk', '$kategori', '$deskripsi', '$harga', '$gender', '$stok', '$ukuran', '$warna', '$fitur', '$foto')";
+
+    if ($koneksi->query($sql) === TRUE) {
+        echo "Produk berhasil ditambahkan!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $koneksi->error;
+    }
+}
